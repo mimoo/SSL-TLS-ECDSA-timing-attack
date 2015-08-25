9b8fa25b4a2ca8ba5225009051eb218debec0eb0
david <david@matasano.com>
2015-08-25 17:55:43 -0500

    corrected nice

--- setup/client/run_client.sh
+++ setup/client/run_client.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm responses.log 2> /dev/null
-sudo nice -n20 ./openssl "$1" & # CPU priority (20)
-sudo schedtool -a 1 $! # CPU affinity (on cpu 1)
+sudo taskset -c 1 nice -n20 ./openssl "$1" # CPU priority (20) and affinity (1)
+
 
 
 
