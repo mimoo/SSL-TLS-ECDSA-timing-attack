3cbb2fb496d0a751d8bae87f0c1a0397389103f5
david <david@matasano.com>
2015-08-25 16:42:14 -0500

    fix only one cpu not two

--- setup/client/run_client.sh
+++ setup/client/run_client.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm responses.log 2> /dev/null
-sudo nice -n20 ./openssl "$1" & # CPU priority
-sudo schedtool -a 0,1 $! # CPU affinity
+sudo nice -n20 ./openssl "$1" & # CPU priority (20)
+sudo schedtool -a 1 $! # CPU affinity (on cpu 1)
 
 
 
